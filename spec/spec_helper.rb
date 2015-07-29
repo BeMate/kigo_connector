65870f98a25c584cd30571d2c97c9377efb5a47d
Diego Fernandez Fernandez <diego.fernandez.fernandez@gmail.com>
2015-07-29 13:37:36 +0200

    Configure VCR so it makes http request if body changes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,7 @@ require_relative "../kigo_connector"
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.allow_http_connections_when_no_cassette = true
+  c.default_cassette_options = { record: :new_episodes, match_requests_on: [:body] }
   c.hook_into :webmock
 end
