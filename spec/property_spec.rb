45994765f067f19da96f926a067b1647ef1cb7a2
Diego Fernandez Fernandez <diego.fernandez.fernandez@gmail.com>
2015-07-30 17:09:50 +0200

    Specs for V2 API calls.

--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -63,4 +63,65 @@ RSpec.describe KigoConnector::Property do
     end
   end
 
+  context "V2 API" do
+
+    describe "#list_properties" do
+
+      it "returns a list of properties" do
+        VCR.use_cassette("list_properties_v2") do
+          properties = KigoConnector::Property.list("2")
+          expect(properties.size).to be > 0
+        end
+      end
+
+    end
+
+    describe "#info" do
+
+      context "for an existing/accesible property" do
+
+        it "returns info for a property" do
+          VCR.use_cassette("property_info_v2") do
+            property = KigoConnector::Property.new(155091, "2")
+            expect(property.info["PROP_NAME"]).to eq "The Lofts at Park Crest  (2 BR)"
+          end
+        end
+
+      end
+
+      context "for a non existing/accesible property" do
+
+        it "raises PropertyNotFound" do
+          VCR.use_cassette("property_info_v2") do
+            property = KigoConnector::Property.new(62637, "2")
+            expect { property.info["PROP_NAME"].to raise_exception KigoConnector::PropertyNotFound }
+          end
+        end
+      end
+    end
+
+    describe "#real_time_pricing_calculation" do
+
+      it "returns price info" do
+        VCR.use_cassette("real_time_pricing_v2", match_requests_on: [:method, :uri]) do
+          property = KigoConnector::Property.new(155091, "2")
+          price_info = property.real_time_pricing_calculaton("2015-08-28", "2015-08-31", 4)
+          expect(price_info["TOTAL_AMOUNT"]).to eq "1766.74"
+        end
+      end
+
+    end
+
+    describe "#periods" do
+
+      it "returns period rates' info" do
+        VCR.use_cassette("property_pricing_setup_v2") do
+          property = KigoConnector::Property.new(155091, "2")
+          expect(property.periods.size).to eq 5
+        end
+      end
+
+    end
+  end
+
 end
