8d946203c8c858a696b359ecb761c555c64e5e33
Diego Fernandez Fernandez <diego.fernandez.fernandez@gmail.com>
2015-07-30 17:09:15 +0200

    Check for date constrains before sending request.

--- lib/property.rb
+++ lib/property.rb
@@ -35,114 +35,123 @@ module KigoConnector
       @rates ||= read_pricing_info
     end
 
+    # checkin and checkout should be passed as a string YYYY-MM-DD
+    # checkin must be greater than today.
+    # checkout must be grater than checkin.
     def real_time_pricing_calculaton(checkin, checkout, guests)
-      response = ApiCall.api_request("computePricing",
-                                     {"PROP_ID": self.id,
-                                      "RES_CREATE":"#{Date.today}",
-                                      "RES_CHECK_IN":"#{checkin}",
-                                      "RES_CHECK_OUT":"#{checkout}",
-                                      "RES_N_ADULTS": guests,
-                                      "RES_N_CHILDREN": 0,
-                                      "RES_N_BABIES": 0},
-                                      api_version)
-      raise PropertyNotFound(response.raw.body) if response.data.nil?
+      if Date.parse(checkin) > Date.today && Date.parse(checkout) > Date.parse(checkin)
+        response = ApiCall.api_request("computePricing",
+                                       {"PROP_ID": self.id,
+                                        "RES_CREATE":"#{Date.today}",
+                                        "RES_CHECK_IN":"#{checkin}",
+                                        "RES_CHECK_OUT":"#{checkout}",
+                                        "RES_N_ADULTS": guests,
+                                        "RES_N_CHILDREN": 0,
+                                        "RES_N_BABIES": 0},
+                                       api_version)
 
-      response.data
-    end
+        raise PropertyNotFound.new(response.raw.body) if response.data.nil?
 
-    %w(fees discounts deposit currency per_request_charge periods).each do |accessor|
-      define_method accessor.to_sym do
-        @rates ||= read_pricing_info
-        instance_variable_get("@#{accessor}")
+        response.data
+      else
+        raise Exception.new("Date constrains not met.")
       end
     end
 
-    private
-
-    class Period
-      attr_reader :check_in, :check_out, :name, :stay_min, :weekly, :nightly_amounts
-
-      def initialize(check_in, check_out, name, stay_min, weekly, nightly_amounts)
-        @check_in = Date.parse(check_in)
-        @check_out = Date.parse(check_out)
-        @name = name
-        @stay_min = stay_min
-        @weekly = weekly
-        @nightly_amounts = nightly_amounts
-      end
-    end
-
-    Fee = Struct.new(:type_id, :include_in_rent, :unit, :value)
-    FeeValue = Struct.new(:stay_from, :unit, :value)
-    PerGuestCharge = Struct.new(:type, :standard, :max)
-
-    def read_info
-      response = ApiCall.api_request("readProperty2", {"PROP_ID": self.id}, api_version)
-      raise PropertyNotFound.new(response.raw.body) if response.data.nil?
-
-      response.data["PROP_INFO"]
-    end
-
-    def read_pricing_info
-      response = ApiCall.api_request("readPropertyPricingSetup", {"PROP_ID": self.id}, api_version)
-      raise PropertyNotFound.new(response.raw.body) if response.data.nil?
-
-      set_currency(response)
-      set_per_guest_charge(response)
-      set_fees(response)
-      set_discounts(response)
-      set_desposit(response)
-      set_periods(response)
-    end
-
-    def set_currency(response)
-      @currency = response.data["PRICING"]["CURRENCY"]
-    end
-
-    def set_per_guest_charge(response)
-      per_guest_charge_info = response.data["PRICING"]["RENT"]["PERGUEST_CHARGE"]
-      @per_guest_charge = PerGuestCharge.new(per_guest_charge_info["TYPE"],
-                                             per_guest_charge_info["STANDARD"],
-                                             per_guest_charge_info["MAX"])
-    end
-
-    def set_fees(response)
-      @fees = []
-      response.data["PRICING"]["FEES"]["FEES"].each do |fee_info|
-
-        if fee_info["VALUE"].is_a? Array
-          fee_values = []
-          fee_info["VALUE"].each do |fee_value_info|
-            fee_values << FeeValue.new(fee_value_info["STAY_FROM"], fee_value_info["UNIT"], fee_value_info["VALUE"])
-          end
-        else
-          fee_values = fee_info["VALUE"]
+      %w(fees discounts deposit currency per_request_charge periods).each do |accessor|
+        define_method accessor.to_sym do
+          @rates ||= read_pricing_info
+          instance_variable_get("@#{accessor}")
         end
-        @fees << Fee.new(fee_info["FEE_TYPE_ID"], fee_info["INCLUDE_IN_RENT"], fee_info["UNIT"], fee_values)
       end
-    end
 
-    def set_discounts(response)
-      @discounts = response.data["PRICING"]["DISCOUNTS"]
-    end
+      private
 
-    def set_desposit(response)
-      @deposit = response.data["PRICING"]["DEPOSIT"]
-    end
+      class Period
+        attr_reader :check_in, :check_out, :name, :stay_min, :weekly, :nightly_amounts
 
-    def set_periods(response)
-      @periods = []
-      response.data["PRICING"]["RENT"]["PERIODS"].each do |period_info|
-        @periods << Period.new(period_info["CHECK_IN"],
-                               period_info["CHECK_OUT"],
-                               period_info["NAME"],
-                               period_info["STAY_MIN"],
-                               period_info["WEEKLY"],
-                               period_info["NIGHTLY_AMOUNTS"])
+        def initialize(check_in, check_out, name, stay_min, weekly, nightly_amounts)
+          @check_in = Date.parse(check_in)
+          @check_out = Date.parse(check_out)
+          @name = name
+          @stay_min = stay_min
+          @weekly = weekly
+          @nightly_amounts = nightly_amounts
+        end
+      end
+
+      Fee = Struct.new(:type_id, :include_in_rent, :unit, :value)
+      FeeValue = Struct.new(:stay_from, :unit, :value)
+      PerGuestCharge = Struct.new(:type, :standard, :max)
+
+      def read_info
+        response = ApiCall.api_request("readProperty2", {"PROP_ID": self.id}, api_version)
+        raise PropertyNotFound.new(response.raw.body) if response.data.nil?
+
+        response.data["PROP_INFO"]
+      end
+
+      def read_pricing_info
+        response = ApiCall.api_request("readPropertyPricingSetup", {"PROP_ID": self.id}, api_version)
+        raise PropertyNotFound.new(response.raw.body) if response.data.nil?
+
+        set_currency(response)
+        set_per_guest_charge(response)
+        set_fees(response)
+        set_discounts(response)
+        set_desposit(response)
+        set_periods(response)
+      end
+
+      def set_currency(response)
+        @currency = response.data["PRICING"]["CURRENCY"]
+      end
+
+      def set_per_guest_charge(response)
+        per_guest_charge_info = response.data["PRICING"]["RENT"]["PERGUEST_CHARGE"]
+        @per_guest_charge =
+          per_guest_charge_info.nil? ? nil : PerGuestCharge.new(per_guest_charge_info["TYPE"],
+                                                                per_guest_charge_info["STANDARD"],
+                                                                per_guest_charge_info["MAX"])
+      end
+
+      def set_fees(response)
+        @fees = []
+        response.data["PRICING"]["FEES"]["FEES"].each do |fee_info|
+
+          if fee_info["VALUE"].is_a? Array
+            fee_values = []
+            fee_info["VALUE"].each do |fee_value_info|
+              fee_values << FeeValue.new(fee_value_info["STAY_FROM"], fee_value_info["UNIT"], fee_value_info["VALUE"])
+            end
+          else
+            fee_values = fee_info["VALUE"]
+          end
+          @fees << Fee.new(fee_info["FEE_TYPE_ID"], fee_info["INCLUDE_IN_RENT"], fee_info["UNIT"], fee_values)
+        end
+      end
+
+      def set_discounts(response)
+        @discounts = response.data["PRICING"]["DISCOUNTS"]
+      end
+
+      def set_desposit(response)
+        @deposit = response.data["PRICING"]["DEPOSIT"]
+      end
+
+      def set_periods(response)
+        @periods = []
+        response.data["PRICING"]["RENT"]["PERIODS"].each do |period_info|
+          @periods << Period.new(period_info["CHECK_IN"],
+                                 period_info["CHECK_OUT"],
+                                 period_info["NAME"],
+                                 period_info["STAY_MIN"],
+                                 period_info["WEEKLY"],
+                                 period_info["NIGHTLY_AMOUNTS"])
+        end
+
       end
 
     end
 
   end
-
-end
